d0e57decb1daa18fe6152d0a460551976aac6753
logongas <lorenzo.profesor@gmail.com>
2015-02-05 01:03:13 +0100

    error en los script de base de datos

--- src/java/es/logongas/iothome/database/V02__device.sql
+++ src/java/es/logongas/iothome/database/V02__device.sql
@@ -1,1 +1,1 @@
-INSERT INTO `iothome`.`device` (`name`, `streamName0`, `streamName1`, `streamName2`) VALUES ('Consumo casa', 'enchufes', 'luces', 'cocina');
+INSERT INTO `device` (`name`, `streamName0`, `streamName1`, `streamName2`, `streamName3`) VALUES ('Consumo casa', 'Cocina', 'Enchufes A', 'Enchufes B','Luces');
